64ab23552cee1a5ebdf2b2eb47738a8c5c64ec4c
Paulo Penteado <pp.penteado@gmail.com>
2019-05-06 14:39:32 -0700

    modified:   util/downloader/download_CASSINI.sh added downloading of ISS PSFs into Cassini/xpsf (~1.8 GB)

--- util/downloader/download_CASSINI.sh
+++ util/downloader/download_CASSINI.sh
@@ -28,9 +28,15 @@ ts=`eval echo "~/.ominas/timestamps/"`
 
 for dir in "${dirs[@]}"
 do
+ #echo ${wget} "${baseurl}${dir}/ --localdir=${1}/$dir/ --absolute --timestamps=$ts $@"
  ${wget} "${baseurl}${dir}/ --localdir=${1}/$dir/ --absolute --timestamps=$ts $@"
 done
 
+echo "Downloading PSFs"
+echo ${wget} "https://pds-imaging.jpl.nasa.gov/data/cassini/cassini_orbiter/coiss_0011_v4.1/calib/xpsf/ --localdir=${1}/xpsf/ -absolute --timestamps=$ts $@"
+${wget} "https://pds-imaging.jpl.nasa.gov/data/cassini/cassini_orbiter/coiss_0011_v4.1/calib/xpsf/ --localdir=${1}/xpsf/ --absolute --timestamps=$ts $@"
+
+exit
 #special treatment directories (spk and ck, which are large)
 echo "Downloading spks"
 ${wget} "${baseurl}spk/ --localdir=${1}/spk/ $@ --absolute --timestamps=$ts " # --xpattern=(\.bsp$)|(\.bsp\.lbl$)"
